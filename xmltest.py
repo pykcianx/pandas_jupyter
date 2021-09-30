36bbec4b3a111d0e0642284cb79e8f2513077141
pykcianx <sznapi221@wp.pl>
2021-09-30 12:45:00 +0200

    new commit

--- xmltest.py
+++ xmltest.py
@@ -1,1 +1,21 @@
-#testa
+import os
+from xml.etree import ElementTree
+
+
+full_file = os.path.abspath(os.path.join('xml', 'reed.xml'))
+dom = ElementTree.parse(full_file)
+courses = dom.findall('course')
+
+for c in courses:
+    title = c.find('title').text
+    num = c.find('crse').text
+    days = c.find('days').text
+
+    print(' *  {}  [{}]  {} '.format(num, days, title))
+
+
+'''
+with open("xml\\reed.xml", "r") as f:
+    data = f.read()
+    print(data)
+'''
